7848921e32f9a8e8ac68d0d289e7fc03f7dd314e
allenlasimo-lab <allenlasimo@gmail.com>
2025-09-16 12:44:41 +0700

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-import React from 'https://esm.sh/react@18';
-import { createRoot } from 'https://esm.sh/react-dom@18/client';
-import App from './app.js';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(React.createElement(App));
